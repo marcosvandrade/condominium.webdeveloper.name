ded350770aa8d3b2f4a4987550cb871d9301d428
Marcos Vasconcellos de Andrade <vascorj2006@gmail.com>
2020-03-03 16:41:12 -0300

    work3mar20

---
+++ spring/sistema-condominio/src/main/java/com/siscondominio/sistemacondominio/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.siscondominio.sistemacondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+}
