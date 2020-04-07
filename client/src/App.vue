5c789eaad03005d64f3d706dd11467c1abd75d46
marcosvandrade <vascorj2006@gmail.com>
2020-04-07 12:30:19 -0300

    home7abr20

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex-container">    
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'App'
+  // components: {
+  //   HelloWorld
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+/* body {
+  background-color: #4b755505;
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  margin: auto;
+  padding: 150px 0;  
+} */
+
+.flex-container {
+  display: flex;
+  background-color: #4b755505;
 }
+
+/* 
+.form-group {
+  
+} */
+
 
 /* @import url(https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap.min.css); */
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router, // inject the router to make whole app router-aware
   render: h => h(App),
 }).$mount('#app')
