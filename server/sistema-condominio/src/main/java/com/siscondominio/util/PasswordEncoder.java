f05c792ed269e4015f72836c0560937a977c6193
marcosvandrade <vascorj2006@gmail.com>
2020-05-27 12:03:03 -0300

    home27maio20

---
+++ server/sistema-condominio/src/main/java/com/siscondominio/util/PasswordEncoder.java
@@ -0,0 +1,10 @@
+package com.siscondominio.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode( "123"));
+    }
+}
