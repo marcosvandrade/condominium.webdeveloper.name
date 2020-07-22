c34a5dd14feeb4c7530c12535adc7a3b395c4498
marcosvandrade <vascorj2006@gmail.com>
2020-07-22 11:44:06 -0300

    home22jul20

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="app">
-    <router-view/>
+  <div id="app">
+    <!-- <router-view/> -->
+    <Login />
   </div>
 </template>
 
 <script>
 
+import Login from "./main/login"
+
 export default {
-    name: 'App'
+    components: { Login }
    }
 
 </script>
 
 <style>
+
+/* @font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+    font-family: "RobotoMono", monospace;
+} */
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: whitesmoke;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
 
 </style>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
-Vue.config.productionTip = false
+import App from './App'
 
 new Vue({
-   router, // inject the router to make whole app router-aware
-   render: h => h(App),
- }).$mount('#app')
+    render: h => h(App)
+}).$mount("#app")
+
+// new Vue({
+//     el: '#app',
+//     render(createElement) {
+//         return createElement(App)
+//     }
+// })
+
