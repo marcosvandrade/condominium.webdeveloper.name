d1f99d8407abc7eadf9c66df78088ac26b055710
Marcos Vasconcellos de Andrade <vascorj2006@gmail.com>
2020-07-29 16:29:41 -0300

    work29jul20

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,12 +21,12 @@ export default {
 <style>
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
     font-family: 'Montserrat', sans-serif;
 }
 
 body {
-    margin: 0;
+  margin: 0;
 }
 
 #app {
@@ -40,6 +40,40 @@ body {
     color: whitesmoke;
     background: linear-gradient(to right, #1f4037, #99f2c8);
 }
+
+#login h3 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 15px;
+    border-radius: 5px;
+}
+
+#login .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+#login .form input {
+  display: flex;
+    padding: 15px;
+    width: 350px;
+}
+
+#login .form label {
+  display: flex;
+  padding: 10px;
+}
+
+#login .form button {
+  display: flex;
+  padding: 10px;
+  margin: 15px;
+  width: 150px;
+  justify-content: center;
+}
+
 
 #register h3 {
     border: 1px solid #000;
@@ -79,6 +113,7 @@ body {
   padding: 8px;
   margin: 10px;
 }
+
 
 </style>
 
