433fb4bab336958609dd4a15f889edd6ca803ee0
marcosvandrade <vascorj2006@gmail.com>
2020-08-01 14:32:40 -0300

    home1ago20

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div id="app">
-    <!-- <router-view/> -->
+  <v-app>
     <Login />
-    <!-- <Cadastro /> -->
-  </div>
+    <!-- <v-main>
+      <router-view/>
+    </v-main> -->
+  </v-app>
 </template>
 
 <script>
@@ -11,11 +12,8 @@
 import Login from "./components/login"
 import Cadastro from "./components/cadastro"
 require('./assets/css/app.css')
-
 export default {
     name: "App",
     components: { Login}
    }
-
 </script>
-
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import vuetify from './plugins/vuetify';
 
 new Vue({
+    vuetify,
     render: h => h(App)
 }).$mount("#app")
 
