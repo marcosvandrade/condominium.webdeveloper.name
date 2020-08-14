bf76c0fb132bf2852b2667b350f7d5abb31e4fa2
marcosvandrade <vascorj2006@gmail.com>
2020-08-14 18:42:09 -0300

    home15ago20

--- server/sistema-condominio/src/main/java/com/siscondominio/endpoint/UsuariosEndpoint.java
+++ server/sistema-condominio/src/main/java/com/siscondominio/endpoint/UsuariosEndpoint.java
@@ -9,7 +9,7 @@ import com.siscondominio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api")
 
