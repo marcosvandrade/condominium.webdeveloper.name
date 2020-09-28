5828bff58b83c6a77a17ac0f6f3f6a44a3f6418c
marcosvandrade <vascorj2006@gmail.com>
2020-09-28 11:31:45 -0300

    configurando responsividade

--- client/src/components/template/Menu.vue
+++ client/src/components/template/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="menu" v-show="isMenuVisible">
+    <div class="menu" v-show="isMenuVisible" v-on="Mobile()">
         <div v-if="user.admin">
             <div class="menu-filter">
                     <i class="fa fa-search fa-lg"></i>
@@ -14,10 +14,6 @@
                                 <path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
                                 <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                             </svg>
-                            <!-- <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-                            </svg> -->
                         </a>
                     </li>
                     <li class="nav-item">
@@ -74,10 +70,6 @@
                                 <path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
                                 <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                             </svg>
-                            <!-- <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-                            </svg> -->
                         </a>
                     </li>
                     <li class="nav-item">
@@ -114,10 +106,27 @@ import { mapState } from 'vuex'
 export default {
     name: 'Menu',
     computed: mapState(['isMenuVisible', 'user']),
+    methods: {
+            Mobile(){
+                if(window.innerWidth <= 990){
+                    // document.getElementById("isMobile").className = "mobile"
+                    // document.createAttribute("mobile")
+                    // document.getElementById('#isMobile').classList.remove('menu');
+                    const mob = document.getElementsByClassName("menu")
+                    mob.className.replace("mobile")
+                    // document.querySelector('menu').className.replace('mobile')
+            }
+        }
+    }
 }
 </script>
 
 <style>
+
+    .mobile a {
+        color: red;
+    }
+
     .menu {
         grid-area: menu;
         width: 100%;
@@ -132,7 +141,7 @@ export default {
     .menu .user-admin a {
         color: white;
     }
-   
+  
     .menu .user-client a {
         color: white;
     }
