52e91a21330f6be37954b2873d5218531fbfffab
marcosvandrade <vascorj2006@gmail.com>
2020-10-12 10:19:49 -0300

    home12out20

--- nodeServer/knexfile.js
+++ nodeServer/knexfile.js
@@ -3,6 +3,8 @@ const { db } = require('./.env')
 module.exports = {
 	client: 'postgresql',
 	connection:{
+		host : 'postgres-instance.clab2bcpgufq.us-east-1.rds.amazonaws.com',
+        port: 5432,
 		database: 'node_siscondominio',
 		user: 'postgres',
 		password: 'Fm6xwUJStQjn'
